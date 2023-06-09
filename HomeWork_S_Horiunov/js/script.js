6f120904a80d928ffc25465400a05a58e310e1c0
SerhiiHoriunov <serghory@gmail.com>
2023-06-09 12:39:14 +0200

    Added files

---
+++ HomeWork_S_Horiunov/js/script.js
@@ -0,0 +1,16 @@
+///////////// вибрати елемент з атрибутом data-widget-name з документа та прочитати його значення.///////////////
+
+// let elem = document.querySelector("[data-widget-name]");
+
+// alert(elem.dataset.widgetName);
+
+// alert(elem.getAttribute("data-widget-name"));
+
+let elemNew = document.getElementById("hook");
+
+alert(elemNew);
+
+elemNew.style.background = "black";
+elemNew.hidden = true;
+
+alert(hook.dataset.widgetName);
